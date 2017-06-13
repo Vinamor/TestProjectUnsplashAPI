46be3272149f899a190d05c52a432a9dccd05769
Binariks <admin@binariks.com>
2017-06-13 14:28:39 +0300

    Initial Commit

---
+++ TeamvoyEntryApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamvoyEntryApp
+//
+//  Created by Ostap Romaniv on 6/13/17.
+//  Copyright © 2017 Ostap Romaniv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
